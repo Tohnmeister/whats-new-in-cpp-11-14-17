a2ee31fbfa16171ef726d90e517a41d7079b2858
Tonni Tielens <tonni.tielens@gmail.com>
2022-05-20 11:30:33 +0200

    Add empty solution and project

---
+++ WhatsNew.cpp
@@ -0,0 +1,35 @@
+// WhatsNew.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+/*
+ * Important topics:
+ * - Smart pointers
+ * - Automatic type deduction
+ * - range based for loops
+ * - Move semantics
+ * - Defaulted and deleted functions
+ * - final and override
+ * - lambda expressions
+ * - std::thread, std::mutex, std::atomic and std::scoped_lock
+ * - list initialization
+ * 
+ * Other interesting topics:
+ * - scoped enums
+ * - new algorithms
+ * - constexpr
+ * - delegating and inherited constructors
+ * - nullptr
+ * - brace or equal initializers
+ * - type aliases
+ * - variadic templates
+ * - unicode string literals
+ * - attributes
+ */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
