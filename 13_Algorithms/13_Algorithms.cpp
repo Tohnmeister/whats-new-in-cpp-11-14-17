53e272a430f2ab3778d9886cf07e4ca6df603fea
Tonni Tielens <tonni.tielens@cpp.canon>
2022-05-23 15:50:50 +0200

    Add algorithms example

---
+++ 13_Algorithms/13_Algorithms.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool isOdd(int i) {
+    return i % 2 != 0;
+}
+
+bool isEven(int i) {
+    return i % 2 == 0;
+}
+
+int main() {
+    // C++11 introduced a few new algorithms for quickly checking if a collection:
+    // - has only elements that satisfy a predicate
+    // - has atleast one element that satisfies a predicate
+    // - has no elements that satisfy a predicate
+
+    std::vector<int> oddNumbers = { 1, 3, 5, 7, 9 };
+
+    // Check if all elements are odd.
+    bool areAllOdd = std::all_of(oddNumbers.begin(), oddNumbers.end(), &isOdd);
+    // Check if it contains any even element.
+    bool containsEven = std::any_of(oddNumbers.begin(), oddNumbers.end(), &isEven);
+    // Check if none are even.
+    bool areNoneEven = std::any_of(oddNumbers.begin(), oddNumbers.end(), &isOdd);
+
+    std::cout << "areAllOdd: " << areAllOdd << "\n";
+    std::cout << "containsEven: " << containsEven << "\n";
+    std::cout << "areNoneEven: " << areNoneEven << "\n";
+}
